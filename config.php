5332783aaca9a72684e60f6d3b7beb9176cb860d
BeboOoZ <36643702+BeboOoZ@users.noreply.github.com>
2018-02-20 00:43:42 +0200

    test files

---
+++ config.php
@@ -0,0 +1,13 @@
+
+        
+        <?php
+		
+		ini_set ('display_errors', 'on')
+		
+		define ("DB_NAME","test201");
+		define ("DB_USERNAME","test201");
+		define ("DB_PASSWORD","Voda@0000");
+		define ("DB_HOST","den1.mysql3.gear.host");
+
+
+        ?>
